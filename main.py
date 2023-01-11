ed6fa5e5f5b67104908463ba521b2236af534224
arcanius <118830188+arcaniuscode@users.noreply.github.com>
2023-01-11 22:29:49 +0700

    add: python essentials

---
+++ main.py
@@ -0,0 +1,21 @@
+from pygame import mixer
+import serial
+import time
+import multiprocessing
+a = True;
+arduino = serial.Serial('COM3', 9600)
+mixer.init()
+def Measure():
+    distance = arduino.readline()
+    return float(distance)
+
+while True:
+    output = Measure()
+    if output < 29.00:
+        if a:
+            mixer.music.load('mixue.mp3')
+            mixer.music.play()
+            a = False
+    else:
+        a = True
+        mixer.music.stop()
